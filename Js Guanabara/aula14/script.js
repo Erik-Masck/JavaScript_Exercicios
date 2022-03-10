5bcf128f303d353eaf1996ab16bbcce8f4e26349
Erik-Masck <erik.masck@gmail.com>
2022-03-10 11:17:10 -0300

    Finalizado
    Exercicio

--- Js Guanabara/aula14/script.js
+++ Js Guanabara/aula14/script.js
@@ -9,13 +9,46 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
-        if (fsex[0].checked){
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        //if e else if para Homem
+        if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'menino.jpg')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'Jhomem.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'Ahomem.jpg')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'idoso.jpg')
+        }
+
+        //if e else if para mulher
         }else if (fsex[1].checked){
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'menina.jpg')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'Jmulher.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'Amulher.jpg')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'idosa.jpg')
+            }
         }
-        //res.style.textAlign = 'center'
+
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 
 }
